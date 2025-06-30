7c21a54416fab676689aaa1f418e028aae92328b
6510110632 <nydukkung@gmail.com>
2025-06-30 13:07:15 +0700

    feat: add put and delete decoration

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI, Request
 from typing import Union
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
 
 @app.get("/hello")
 def hello_world():
@@ -12,7 +17,14 @@ def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
 @app.post("/items")
-async def create_item(request: Request):
-    body = await request.json()
-    print(body["name"])
-    return {"request body": body}
+def create_item(item: Item):
+    print(item.name, item.price)
+    return {"request body": item}
+
+@app.put("/items/{item_id}")
+def edit_item(item_id: int, item: Item):
+    return {"id": item_id, "request body": item}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": f"Item {item_id} deleted"}
